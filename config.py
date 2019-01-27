c90660e6a9c86f05ad9505e2779d862dd5a3eb2b
Mateusz Koczan <xmateuszkoczan@gmail.com>
2019-01-27 22:26:24 +0100

    Update config

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config(object):
     ENV='production'
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SESSION_COOKIE_HTTPONLY=True
     SESSION_COOKIE_SECURE=True
     PREFERRED_URL_SCHEME='https'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql+psycopg2://databaseuser:P@ssw0rd@localhost/the_database'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
